9d5197e33f2ca457399ec23b47587b70bc9e2504
ony19161 <aman.19161@gmail.com>
2023-03-20 01:08:50 +0600

    solved weighted string problem

--- ony19161/weighted-string/main.cpp
+++ ony19161/weighted-string/main.cpp
@@ -1,51 +1,70 @@
-  #include <iostream>
+#include <iostream>
 #include <cmath>
 #include <bits/stdc++.h>
 #include <string>
 #include <algorithm>
 using namespace std;
 
-
-
 /*
-	Problem Link: https://rb.gy/gebfuo
+    Problem Link: https://rb.gy/gebfuo
 */
 
-vector<string> weightedUniformStrings(string s, vector<int> queries) {
+vector<string> weightedUniformStrings(string s, vector<int> queries)
+{
+    vector<string> results;
+    vector<int> weights;
+    int stringWeight = 0;
 
+    for (int i = 0; i < s.length(); i++)
+    {
+
+        if (s[i] != s[i + 1])
+        {
+            stringWeight = stringWeight + (s[i] - 97) + 1;
+            weights.push_back(stringWeight);
+            stringWeight = 0;
+        }
+        else
+        {
+            stringWeight = stringWeight + (s[i] - 97) + 1;
+            weights.push_back(stringWeight);
+        }
+    }
+    for (unsigned int i = 0; i < queries.size(); i++)
+    {
+        if (find(weights.begin(), weights.end(), queries[i]) != weights.end())
+        {
+            results.push_back("Yes");
+        }
+        else
+        {
+            results.push_back("No");
+        }
+    }
+
+    return results;
 }
 
 int main()
 {
 
-    vector<int> myvector;
-    
-    //string password = "4700";
+    // string password = "4700";
     string password = "Ab1";
-    string value = "abccddde";
-    int stringWeight = 0;
-    for (int i = 0; i < value.length() - 1; i++) {
-        char temp = value[i];
-        
+    string value = "abbcccdddd";
+    vector<int> queries;
+    queries.push_back(1);
+    queries.push_back(7);
+    queries.push_back(5);
+    queries.push_back(4);
+    queries.push_back(15);
 
-        if (value[i] != value[i+1]) {
-            stringWeight = stringWeight + (value[i] - 97) + 1;
-            myvector.push_back(stringWeight);
-            stringWeight = 0;
-        } else {
-            stringWeight = stringWeight + (value[i] - 97) + 1;
-        }
-    }
+    vector<string> results = weightedUniformStrings(value, queries);
 
-    //vector<string> result = weightedUniformStrings(value, myvector);
-    
-    for(unsigned int i = 0; i < myvector.size(); i++)
+    for (unsigned int i = 0; i < results.size(); i++)
     {
-        cout << myvector[i] << " ";
+        cout << results[i] << " ";
     }
-    //cout << value << endl;
-
-   
+    // cout << value << endl;
 
     return 0;
 }
